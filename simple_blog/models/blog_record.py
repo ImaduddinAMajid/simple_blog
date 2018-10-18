73623b84b49762f41553212ffa8b22fc5ad1ef6b
Imaduddin A Majid <imaduddinamajid@gmail.com>
2018-10-18 19:46:56 +0300

    Add user and blog record table (model)

---
+++ simple_blog/models/blog_record.py
@@ -0,0 +1,7 @@
+import datetime
+from sample_blog.models.meta import Base
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import Unicode
+from sqlalchemy import UnicodeText
+from sqlalchemy import DateTime
