fee37b663f5c63f63f0702360a7f6fa70e249d42
Imaduddin A Majid <imaduddinamajid@gmail.com>
2018-10-18 18:49:58 +0300

    Add project dependencies

--- setup.py
+++ setup.py
@@ -9,17 +9,20 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
 requires = [
-    'plaster_pastedeploy',
-    'pyramid >= 1.9a',
-    'pyramid_debugtoolbar',
+    'pyramid >= 1.9',
     'pyramid_jinja2',
-    'pyramid_retry',
+    'pyramid_debugtoolbar',
     'pyramid_tm',
-    'SQLAlchemy',
+    'pyramid_retry',
+    'SQLAlchemy>=1.0',
     'transaction',
     'zope.sqlalchemy',
     'waitress',
-]
+    'wtforms==2.1',
+    'webhelpers2==2.0',
+    'paginate==0.5.6',
+    'paginate_sqlalchemy==0.2.0'
+    ]
 
 tests_require = [
     'WebTest >= 1.3.1',  # py3 compat
