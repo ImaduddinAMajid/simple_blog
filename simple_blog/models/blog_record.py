2924912e3f042b7d4e6ccae6b4be748958fe0bfa
Imaduddin A Majid <imaduddinamajid@gmail.com>
2018-10-18 20:14:21 +0300

    User and Blog record model

--- simple_blog/models/blog_record.py
+++ simple_blog/models/blog_record.py
@@ -1,7 +1,15 @@
 import datetime
-from sample_blog.models.meta import Base
+from simple_blog.models.meta import Base
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import Unicode
 from sqlalchemy import UnicodeText
 from sqlalchemy import DateTime
+
+
+class BlogRecord(Base):
+	__tablename__ = 'entries'
+	id = Column(Integer, primary_key=True)
+	title = Column(Unicode(255), unique=True, nullable=False)
+	body = Column(UnicodeText, default=u'')
+	created = Column(DateTime, default=datetime.datetime.utcnow)	
--- simple_blog/models/user.py
+++ simple_blog/models/user.py
@@ -1,7 +1,14 @@
 import datetime
-from sample_blog.models.meta import Base
+from simple_blog.models.meta import Base
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import Unicode
 from sqlalchemy import UnicodeText
 from sqlalchemy import DateTime
+
+class User(Base):
+	__tablename__ = 'users'
+	id = Column(Integer, primary_key=True)
+	name = Column(Unicode(255), unique=True, nullable=False)
+	password = Column(Unicode(255), nullable=(False))
+	last_logged = Column(DateTime, default=datetime.datetime.utcnow)
