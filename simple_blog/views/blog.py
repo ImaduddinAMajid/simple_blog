378ea1751548edc251f89fd35190e4445f9837ad
Imaduddin A Majid <imaduddinamajid@gmail.com>
2018-10-19 07:30:13 +0300

    add create blog entry view

--- simple_blog/views/blog.py
+++ simple_blog/views/blog.py
@@ -2,6 +2,7 @@ from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPNotFound, HTTPFound
 from ..models.blog_record import BlogRecord
 from ..services.blog_record import BlogRecordService
+from ..forms import BlogCreateForm, BlogUpdateForm
 
 @view_config(route_name='blog',
 renderer='simple_blog:templates/view_blog.jinja2')
@@ -15,7 +16,14 @@ def blog_view(request):
 @view_config(route_name='blog_action', match_param='action=create',
 renderer='simple_blog:templates/create_blog.jinja2')
 def blog_create(request):
-	return{}
+	entry = BlogRecord()
+	form  = BlogCreateForm(request.POST)
+	if request.method == 'POST' and form.validate():
+		form.populate_obj(entry)
+		request.dbsession.add(entry)
+		return HTTPFound(location=request.route_url('home'))
+	return {'form': form, 'action': request.matchdict.get('action')}
+
 
 @view_config(route_name='blog_action', match_param='action=edit',
 renderer='simple_blog:templates/edit_blog.jinja2')
