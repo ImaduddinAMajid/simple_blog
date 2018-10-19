568f329a0331a0c9952752650e83c74ab912250c
Imaduddin A Majid <imaduddinamajid@gmail.com>
2018-10-19 09:37:15 +0300

    Add authentication and registration

--- simple_blog/views/auth.py
+++ simple_blog/views/auth.py
@@ -1,6 +1,33 @@
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+from pyramid.security import remember, forget
+from ..services.user import UserService
+from ..forms import RegistrationForm
+from ..models.user import User
+
 
 @view_config(route_name='auth', match_param='action=in', renderer='string', request_method='POST')
 @view_config(route_name='auth', match_param='action=out', renderer='string')
 def sign_in_out(request):
-	return{}
+    username = request.POST.get('username')
+    if username:
+        user = UserService.by_name(username, request=request)
+        if user and user.verify_password(request.POST.get('password')):
+            headers = remember(request, user.name)
+        else:
+            headers = forget(request)
+    else:
+        headers = forget(request)
+    return HTTPFound(location=request.route_url('home'), headers=headers)
+
+
+@view_config(route_name='register',
+             renderer='simple_blog:templates/register.jinja2')
+def register(request):
+    form = RegistrationForm(request.POST)
+    if request.method == 'POST' and form.validate():
+        new_user = User(name=form.username.data)
+        new_user.set_password(form.password.data.encode('utf8'))
+        request.dbsession.add(new_user)
+        return HTTPFound(location=request.route_url('home'))
+    return {'form': form}
